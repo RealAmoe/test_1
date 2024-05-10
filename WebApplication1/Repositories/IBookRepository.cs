13185a90aee0008604bbee85f6724ea5bc59d2f2
RealAmoe <116590501+RealAmoe@users.noreply.github.com>
2024-05-10 19:01:25 +0200

    Delete WebApplication1 directory

--- WebApplication1/Repositories/IBookRepository.cs
+++
@@ -1,9 +0,0 @@
-using WebApplication1.Models;
-
-namespace WebApplication1.Repositories;
-
-public interface IBookRepository
-{
-    public List<BookEditionDTO> Books(int BookId);
-    public int AddBook(AddBookDTO addBookDto);
-}
--- WebApplication1/Services/IBookService.cs
+++
@@ -1,9 +0,0 @@
-using WebApplication1.Models;
-
-namespace WebApplication1.Services;
-
-public interface IBookService
-{
-    public List<BookEditionDTO> Books(int BookId);
-    public int AddBook(AddBookDTO addBookDto);
-}
